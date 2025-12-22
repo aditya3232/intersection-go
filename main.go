35e1d9cb0fa532e70c6f66ec381660722801c8b3
adit3232 <m.aditya3232@gmail.com>
2025-12-22 11:52:24 +0700

    menambahkan handle http sederhana

--- main.go
+++ main.go
@@ -3,9 +3,26 @@ package main
 import (
 	"fmt"
 	"intersection-go/intersection"
+	"net/http"
 )
 
 func main() {
-	result := intersection.FindIntersection([]string{"1,3,4,7,13", "1,2,4,13,15"})
-	fmt.Println("hasil intersection antara [\"1,3,4,7,13\", \"1,2,4,13,15\"] adalah: ", result)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		result := intersection.FindIntersection([]string{
+			"1,3,4,7,13",
+			"1,2,4,4,13,15",
+		})
+
+		fmt.Fprintf(
+			w,
+			"hasil intersection antara [1,3,4,7,13] dan [1,2,4,13,15] adalah: %s\n",
+			result,
+		)
+	})
+
+	fmt.Println("server running on :3232")
+	err := http.ListenAndServe(":3232", nil)
+	if err != nil {
+		panic(err)
+	}
 }
